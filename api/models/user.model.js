8a3562968d4c827b7c65bdca21ba95d41b3aeb2c
wynehtetaung <wynehtet2022@gmail.com>
2024-07-31 12:14:16 +0630

    [create] models and user.model.js

---
+++ api/models/user.model.js
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("users", userSchema);
+export default User;
